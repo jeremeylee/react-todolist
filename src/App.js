a695eaa96a9200a2d781b1621c5444841111b3c2
Jeremey Lee <leejeremey@gmail.com>
2019-06-26 13:14:07 -0700

    Added delete, post, and get HTTP requests. Edit/post request not finished.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import todoServices from './services/todos'
 
 function App() {
   const [todos, setTodos] = useState([])
   const [newItem, setNewItem] = useState('')
   
+  useEffect(() => {
+    console.log('effect')
+    todoServices
+      .getAll()
+      .then(initialTodos => {
+        console.log('promise fulfilled')
+        setTodos(initialTodos)
+      })
+  }, [])
+
+  console.log(`rendered ${todos.length} items`)
+
   const todoList = () => 
     todos.map(todo =>
       <li>
         {todo.todo}
+        <button onClick={() => deleteItem(todo.id)}>delete</button>
+        <button onClick={() => editItem(todo.id)}>edit</button>
       </li>
     )
   
-
+  const deleteItem = id => {
+    todoServices
+      .deleteItem(id)
+      .then(() => {
+        setTodos(todos.filter(todo => todo.id !== id))
+      })
+  }
+  
+  const editItem = id => {
+    const todo = todos.find(x => x.id == id)
+    setNewItem(todo.todo)
+  }
   const addItem = (event) => {
     event.preventDefault()
 
     const itemObject = {
       todo: newItem
     }
-
-    setTodos(todos.concat(itemObject))
-
+    todoServices
+      .create(itemObject)
+      .then(returnedTodos => {
+        setTodos(todos.concat(returnedTodos))
+        setNewItem('')
+      }) 
   }
 
   const handleTodoChange = (event) => {
@@ -43,7 +72,7 @@ function App() {
         <button type="submit">Add</button>
       </form>
     </div>
-  );
+  )
 }
 
 export default App;
